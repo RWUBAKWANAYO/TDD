058bdceda111fb93dfeb4b979af75bf4e990968a
kamzzy <preciousakams@yahoo.com>
2022-08-06 00:01:43 +0100

    code refractored and all tests passing

--- solver.rb
+++ solver.rb
@@ -1,11 +1,11 @@
 class Solver
   def fizzbuzz(number)
-    if (number % 3).zero?
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
       'fizz'
     elsif (number % 5).zero?
       'buzz'
-    elsif (number % 3 == 0) && (number % 5 == 0)
-      'fizzbuzz'
     else
       number.to_s
     end
