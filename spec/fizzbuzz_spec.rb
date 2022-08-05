4a6a664e55a4040f8588750754ae64c091da9cdd
kamzzy <preciousakams@yahoo.com>
2022-08-05 23:36:49 +0100

    1 passing test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../solver'
-describe '.fizzbuzz' do
-  it 'should return string "Fizz" if number is divisible by 3' do
-    Solver.fizzbuzz(number) % 3 == 0 ? 'fizz' : number.to_s
-    expect(Solver.fizzbuzz(3)).to eq 'fizz'
+
+describe '#fizzbuzz' do
+  it 'should return string "fizz" if number is divisible by 3' do
+    expect(Solver.new.fizzbuzz(3)).to eq 'fizz'
   end
   it 'should return string "Buzz" if number is divisible by 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
