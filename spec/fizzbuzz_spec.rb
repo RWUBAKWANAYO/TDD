edffc82a274fefbe88bdd2843bc8e41f4a6f3f92
kamzzy <preciousakams@yahoo.com>
2022-08-05 23:37:52 +0100

    2 passing tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,7 +5,7 @@ describe '#fizzbuzz' do
     expect(Solver.new.fizzbuzz(3)).to eq 'fizz'
   end
   it 'should return string "Buzz" if number is divisible by 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(Solver.new.fizzbuzz(5)).to eq 'buzz'
   end
   it 'should return string "FizzBuzz" if number is divisible by 3 and 5' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
