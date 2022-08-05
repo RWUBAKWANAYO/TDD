885976bc256310f4653518ea244b4fe44dd1e9e8
kamzzy <preciousakams@yahoo.com>
2022-08-05 22:55:47 +0100

    failing test

--- solver.rb
+++ solver.rb
@@ -1,3 +1,3 @@
 class Solver
-
+  def fizzbuzz; end
 end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,6 @@
+require_relative '../solver'
+describe '.fizzbuzz' do
+  it 'should take a number and return a string' do
+    expect(fizzbuzz(1)).to eq String
+  end
+end
